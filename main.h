be7cb8d9043caffba61cea7421cd42ca257946d6
MarouaYM <marouayam@gmail.com>
2023-05-31 09:09:44 -0700

    0x11. C - printf

--- main.h
+++ main.h
@@ -39,15 +39,9 @@
 
 
 /**
-
  * struct fmt - Struct op
-
- *
-
  * @fmt: The format.
-
  * @fn: The function associated.
-
  */
 
 struct fmt
@@ -65,15 +59,9 @@ struct fmt
 
 
 /**
-
  * typedef struct fmt fmt_t - Struct op
-
- *
-
  * @fmt: The format.
-
  * @fm_t: The function associated.
-
  */
 
 typedef struct fmt fmt_t;
